f03a7cec69896096451645c01d69f2d0a13b11d3
Caleb H. <147122942+Chsie012@users.noreply.github.com>
2024-12-17 20:55:56 -0800

    Update server.js
    Updated server.js file to enable Heroku app

--- server.js
+++ server.js
@@ -330,6 +330,6 @@ const server = http.createServer((req, res) => {
   });
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 server.listen(port, () => console.log('Server is listening on port', port));
